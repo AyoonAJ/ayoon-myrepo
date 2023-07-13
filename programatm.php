2fb2019ead7bba3ba67c01c8910585e1544cdf0b
AyoonAJ <139105405+AyoonAJ@users.noreply.github.com>
2023-07-14 01:41:24 +0800

    Update programatm.php

--- programatm.php
+++ programatm.php
@@ -1,4 +1,6 @@
 <?php
+
+//fungsi untuk menampilkan pilihan menu atm (akan dipanggil di program utama)
 function tampilkanMenu(){
     echo "===== Pilih Menu =====\n";
     echo "1. Cek saldo\n";
@@ -7,6 +9,26 @@ function tampilkanMenu(){
     echo "4. Keluar\n";
     echo "Silakan pilih menu diatas: ";
 }
+
+//fungsi/aksi untuk cek saldo (akan dipanggil di program utama)
+function cekSaldo($saldo)
+{
+    echo "Saldo Anda: " . $saldo . "\n";
+}
+
+//fungsi untuk setor tunai (akan dipanggil di program utama)
+function setorTunai($saldo){
+    echo "Selamat datang di menu setor tunai\n";
+    echo "=================================\n\n";
+    echo "Masukkan nominal setor: ";
+    $setor = (int) readline();
+    $saldo += $setor;
+    return $saldo;
+}
+
+//fungsi untuk tarik tunai (akan dipanggil di program utama)
+
+//fungsi untuk keluar
 
 //Program utama
 echo "Selamat datang di mesin atm BKI\n";
@@ -19,7 +41,7 @@ while(true){
             echo "Ini adalah menu cek saldo\n";
             break;
         case 2:
-            echo "Ini adalah menu setor tunai\n";
+            $saldo = setorTunai($saldo)
             break;
         default:
             echo "Pilihan tidak valid\n";
